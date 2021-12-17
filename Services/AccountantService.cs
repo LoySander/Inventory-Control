f617079665b51dc7226af28e3e841291861d4a2f
Ignat Churilo <i.ignatch.com@mail.ru>
2021-12-17 19:02:30 +0300

    Implement Accountant service

---
+++ Services/AccountantService.cs
@@ -0,0 +1,38 @@
+﻿using Model.Repositories.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseAccountingSystem;
+
+namespace Services
+{
+    public class AccountantService
+    {
+        private IOrderDao orderDao;
+
+        public AccountantService()
+        {
+            orderDao = ListOrderDao.getInstance();
+        }
+
+        public int calculateIncome()
+        {
+            return orderDao.getAllOrders()
+                .Where(order => order.OrderType == OrderType.client
+                    && (order.StatusOrder.Equals("Оплачено")
+                    || order.StatusOrder.Equals("Передан в службу доставки")
+                    || order.StatusOrder.Equals("Доставлен")))
+                .Sum(order => order.TotalCost);
+        }
+
+        public int calculateCosts()
+        {
+            return orderDao.getAllOrders()
+                .Where(order => order.OrderType == OrderType.provider 
+                    && order.StatusOrder.Equals("Оплачено"))
+                .Sum(order => order.TotalCost);
+        }
+    }
+}
